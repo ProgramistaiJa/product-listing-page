d43e7fea98b7d046f0b3e3dee876e4e8b5cba77c
ProgramistaiJa <programista@i-ja.pl>
2018-05-06 17:36:11 +0200

    Added filtering function

--- src/components/ProductsList/index.js
+++ src/components/ProductsList/index.js
@@ -4,41 +4,52 @@ import { withStyles } from 'material-ui/styles';
 import {products} from '../../utilitys/products';
 import Product from './Product';
 import Grid from 'material-ui/Grid';
+import { styles } from './style'
 
-const styles = theme => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        height: 140,
-        width: 100,
-    },
-    control: {
-        padding: theme.spacing.unit * 2,
-    },
-});
+// const styles = theme => ({
+//     root: {
+//         flexGrow: 1,
+//     },
+//     paper: {
+//         height: 140,
+//         width: 100,
+//     },
+//     control: {
+//         padding: theme.spacing.unit * 2,
+//     },
+// });
+
+
 
 class ProductsList extends React.Component {
+
+    categoriesFilter(product, categories){
+        return categories.length > 0
+            ? categories.some(category => category === product.category)
+            : true
+    }
+    colorsFilter(product, colors){
+        colors.length>0 && console.log(product[`${colors[0]}`]);
+
+        return colors.length > 0
+            ? colors.some(color => product[`${color}`] )
+            : true
+    }
     render () {
         const { classes } = this.props;
-        const categories = ["bags"];
+        const categories = [];
+        const colors = ["scout", "jetty"];
         return (
             <Grid container className={classes.root} spacing={16}>
                 <Grid item xs={12}>
                     <Grid container justify="center" spacing={16}>
-                        {console.log(categories[0])}
-
-                        {console.log(products[4].category)}
                         {
                             products
-                                .map(product=>console.log(product.category === categories[0]))
-                        }
-                        {
-                            products
-                                .filter(product => product.category === categories[0] )
+                                .filter(product => this.categoriesFilter(product, categories))
+                                .filter(product => this.colorsFilter(product, colors))
                                 .map(product=>(
-                                <Product key={product.name} product={product} />
-                            ))
+                                    <Product key={product.name} product={product} />
+                                ))
                         }
                     </Grid>
                 </Grid>
