b69f87ff7e1b7e786acae574b81fa75a649fb6b5
ProgramistaiJa <programista@i-ja.pl>
2018-05-07 08:21:42 +0200

    Added actions and reducers

--- src/reducers/color.reducer.js
+++ src/reducers/color.reducer.js
@@ -1,7 +1,7 @@
 import {actionsConstans} from '../utilitys/constants/filters';
 
 export const color = (state = [], action) => {
-    console.log(action);
+    // console.log(action);
     switch (action.type) {
         case actionsConstans.ADD_COLOR:
             return action.color;
